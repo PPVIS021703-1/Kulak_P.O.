9214b58d4a93283f2ee1f0697adbfe5c0525fe24
Prohor Kulak <40538451+lagatun54@users.noreply.github.com>
2021-11-20 02:49:59 +0300

    .

--- lab3/operation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"user.h"
-class operation:public user                    //класс выполнения операций пользователем
-{
-public:
-	void addordeletemoneytofromdepozit();  //метод добавления или снятия денег с депозита
-	void printbalance();//метод просмотра баланса депозита
-};
-
