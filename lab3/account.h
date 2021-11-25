a94cf6d81b228d8471e62362962f8ce0bd24fb2d
Prohor Kulak <40538451+lagatun54@users.noreply.github.com>
2021-11-25 04:45:17 +0300

    Delete lab3 directory

--- lab3/account.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"user.h"
-
-class account:public user     //класс ввода информации о пользователе
-{
-public:
-	void enter();
-};
-
--- lab3/interface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include"user.h"
-class interface:public user   //класс отбражения всей информации пользователя
-{
-public:
-	void database();
-};
